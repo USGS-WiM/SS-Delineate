4c6f7cc33e9aa3c93426e45e8d98b835f321d2fb
Marty <marsmith@usgs.gov>
2019-11-21 13:37:40 -0500

    edit

--- run.php
+++ run.php
@@ -3,7 +3,7 @@
 $x_coord = $_GET['x'];
 $y_coord = $_GET['y'];
 
-$command = './app.py ' . $x_coord . ' ' . $y_coord;
+$command = './php_app.py ' . $x_coord . ' ' . $y_coord;
 $output = passthru($command);
 echo $output;
 
